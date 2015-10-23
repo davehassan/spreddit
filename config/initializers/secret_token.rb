862ba1a4f3399f1ca113d77117346bed432af8c3
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2015-10-23 10:56:59 -0400

    Init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditClone::Application.config.secret_key_base = '0e36219c993a842f381c63e7e785ccade7cc86ad3226ac17870549701c625f239e4316d2531e71f3a17f20b36fce8ba9df8c93f31d7dfaae2799e0faeb966197'
